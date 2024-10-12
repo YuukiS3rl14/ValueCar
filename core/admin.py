f888eb457d2b9c3cb07a661a6f834c24ec65ea71
Manuel Valenzuela <yxds3rl14@gmail.com>
2024-10-12 16:59:19 -0300

    creacion de modelos

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,11 @@ from .models import *
 
 # Register your models here.
 
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'numero', 'nombre']
+    search_fields = ['id', 'numero', 'nombre']
+    list_per_page = 10
+
 class ComunaAdmin(admin.ModelAdmin):
     list_display = ['id', 'region', 'nombre']
     search_fields = ['id', 'region', 'nombre']
@@ -18,14 +23,14 @@ class ProductoAdmin(admin.ModelAdmin):
     search_fields = ['id', 'supermercado', 'marca', 'precio', 'descripcion', 'fecha_actualizacion']
     list_per_page = 10
 
-class TipoUsuarioAdmin(admin.ModelAdmin):
+class RolAdmin(admin.ModelAdmin):
     list_display = ['id', 'nombre']
     search_fields = ['id', 'nombre']
     list_per_page = 10
 
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ['tipousuario', 'nombre', 'apellido', 'email']
-    search_fields = ['tipousuario', 'nombre', 'apellido', 'email']
+    list_display = ['rol', 'nombre', 'apellido', 'email']
+    search_fields = ['rol', 'nombre', 'apellido', 'email']
     list_per_page = 10
 
 class ComentarioAdmin(admin.ModelAdmin):
@@ -33,9 +38,10 @@ class ComentarioAdmin(admin.ModelAdmin):
     search_fields = ['titulo', 'texto', 'calificacion', 'usuario', 'producto']
     list_per_page = 10
 
+admin.site.register(Region, RegionAdmin)
 admin.site.register(Comuna, ComunaAdmin)
 admin.site.register(Supermercado, SupermercadoAdmin)
 admin.site.register(Producto, ProductoAdmin)
-admin.site.register(TipoUsuario, TipoUsuarioAdmin)
+admin.site.register(Rol, RolAdmin)
 admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Comentario, ComentarioAdmin)
--- core/models.py
+++ core/models.py
@@ -1,46 +1,52 @@
-from django.core.validators import MaxValueValidator
 from django.db import models
 
 # Create your models here.
 
+class Region(models.Model):
+    numero = models.IntegerField()
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
 class Comuna(models.Model):
-    region = models.CharField(max_length=20)
-    nombre = models.CharField(max_length=20)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
 
 class Supermercado(models.Model):
-    nombre = models.CharField(max_length=20)
     comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
 
 class Producto(models.Model):
     supermercado = models.ForeignKey(Supermercado, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=20)
-    marca = models.CharField(max_length=20)
-    precio = models.IntegerField(validators=[MaxValueValidator(999999)]) 
-    descripcion = models.CharField(max_length=200)
-    imagen = models.CharField(max_length=200)
-    origen = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    precio = models.IntegerField() 
+    descripcion = models.CharField(max_length=300)
+    imagen_url = models.CharField(max_length=300)
+    origen_url = models.CharField(max_length=300)
     fecha_actualizacion = models.DateField()
 
     def __str__(self):
         return self.nombre 
     
-class TipoUsuario(models.Model):
-    nombre = models.CharField(max_length=30)
+class Rol(models.Model):
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre 
     
 class Usuario(models.Model):
-    tipousuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.CharField(max_length=300)
     contraseña = models.CharField(max_length=256)
 
     def __str__(self):
@@ -49,7 +55,7 @@ class Usuario(models.Model):
 class Comentario(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=50)
-    calificacion = models.IntegerField(validators=[MaxValueValidator(999999)]) 
+    calificacion = models.IntegerField() 
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
 
